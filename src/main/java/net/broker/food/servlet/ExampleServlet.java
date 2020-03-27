43ec2e99a02dd9cf123eb5d5f3d1a3c2241ac488
egrellis <edwardellis.egre@gmail.com>
2020-03-27 20:46:13 +0000

    Added a simple Java servlet

---
+++ src/main/java/net/broker/food/servlet/ExampleServlet.java
@@ -0,0 +1,15 @@
+package net.broker.food.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ExampleServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter htmlOutput = response.getWriter();
+        htmlOutput.println("<html><head></head><body><p>This html was from the servlet, here is a random number: "+(Math.round(Math.random()*10))+"</p></body></html>");
+    }
+}
