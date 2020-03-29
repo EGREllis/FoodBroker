5002baaa0dce30dc54c036b4df2ebe3cd09f0517
egrellis <edwardellis.egre@gmail.com>
2020-03-29 19:51:30 +0100

    Modified release script

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
-mvn clean install
+/website/tools/maven/apache-maven-3.6.3/bin/mvn clean install
 cp ./target/food.war ../../tools/javaee/glassfish5/glassfish/domains/domain1/autodeploy/
---
+++ src/main/java/net/broker/food/servlet/ProcessDemandServlet.java
@@ -0,0 +1,4 @@
+package net.broker.food.servlet;
+
+public class ProcessDemandServlet {
+}
