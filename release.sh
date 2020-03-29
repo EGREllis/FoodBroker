9e324d9955a583880d5d3de17b171015acc9ece7
egrellis <edwardellis.egre@gmail.com>
2020-03-29 19:53:39 +0100

    fixed release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+PATH="/website/tools/jdk/jdk1.8.0_241/bin:$PATH"
+JAVA_HOME="/website/tools/jdk/jdk1.8.0_241/"
+M2_HOME="/website/tools/maven/apache-maven-3.6.3"
+M2_REPO="/website/tools/repo"
+PATH="/website/tools/maven/apache-maven-3.6.3/bin:$PATH"
+
 git pull
 /website/tools/maven/apache-maven-3.6.3/bin/mvn clean install
 cp ./target/food.war ../../tools/javaee/glassfish5/glassfish/domains/domain1/autodeploy/
